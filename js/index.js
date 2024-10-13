49084ac82cc835e8405b39f3f6faf1a522196fc3
ChabbehAymen <devaymen15.5.3@gmail.com>
2024-10-13 11:58:42 +0100

    col select option selection

--- js/index.js
+++ js/index.js
@@ -2,7 +2,48 @@ $(document).ready(function () {
   // $(".reset").on("click", resetFilter );
   $(".save").on("click", fetchData);
   $(".checkbox .data .list").children().each(function (option) {$(this).on("click", function () { formCheckboxUnique($(this))});});
+  $("#cont > div > .op > .side > .select.sortCol > .data > .list > .input > .data > input").off("input").on("input", function(event) { sortColSelectFilter($(event.target).parents(".select")); });
+  $("#cont > div > .op > .side > .select.sortCol > .data > .list > .option").not(".readonly").children("a").off("click").on("click", function(event) { sortColSelectOption($(event.target).parents(".option")); });
 });
+/**
+ * Selects an option from sortCol select
+ * @param {HTMLElement} div 
+ */
+function sortColSelectOption(div)
+{
+    formSelectOption(div);
+    sortColAdapt();
+    var sortCol = $(div).attr("code"); if(sortCol == undefined || sortCol == null || sortCol == "") sortCol = "dossierCode";
+    var obj = { index: { sortCol: sortCol } };
+}
+/**
+ * displays the name and root of selected option 
+ */
+function sortColAdapt()
+{
+    var code = $("#cont > div > .op > .side > .select.sortCol").attr("code"); if(code == undefined || code == null) code = "";
+    var option = $("#cont > div > .op > .side > .select.sortCol > .data > .list > .option[code='" + code + "']");
+    if(option == undefined || option == null || option == "" || option.length == 0) return;
+    var parent = $(option).attr("parent"); if(parent == undefined || parent == null || parent == "") return;
+    var txt = [ $(option).siblings(".option[code='" + parent + "']").children("a").text(), $(option).children("a").text() ].filter(e => e != "").join(" <i class='fa-solid fa-angle-right'></i> ");
+    $(option).parents(".select").children(".data").children("a").children(".main").children(".txt").html(txt);
+}
+/**
+ * filters select options from the inner input of select
+ * @param {HTMLElement} div 
+ */
+function sortColSelectFilter(div)
+{
+    formSelectFilter(div);
+    $(div).children(".data").children(".list").children(".option").not(".off").each(function()
+    {
+        if($(this).css("display") == "none") return;
+        var code = $(this).attr("code"); if(code == undefined || code == null || code == "") return;
+        var parent = $(this).attr("parent"); if(parent == undefined || parent == null) parent = "";
+        if(parent == "") $(div).children(".data").children(".list").children(".option[parent='" + code + "']").show();
+        else $(div).children(".data").children(".list").children(".option[code='" + parent + "']").show();
+    });
+}
 /**
  * Shows popup the button of each row in the index table
  * @param {HTMLElement} div 
